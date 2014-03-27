ab0055b3258a0c3011a25268bb0755a6ceb87a7d
ElektraS <pallas.aurelie@gmail.com>
2014-03-27 10:10:17 +0100

    fiche company 27/03 2

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,3 @@
 class Company < ActiveRecord::Base
 	has_many :internship
-	has_many :user
 end
--- app/models/internship.rb
+++ app/models/internship.rb
@@ -1,3 +1,5 @@
 class Internship < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :company
 	include PgSearch
 end
